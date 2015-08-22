fef345b968067eac3e6c546c59bfd3847434839c
Anton Ekblad <anton@ekblad.cc>
2015-08-22 13:22:52 +0200

    --haste should go last when hacking the command line.

--- cabal-install/Main.hs
+++ cabal-install/Main.hs
@@ -188,7 +188,7 @@ main = do
 #endif
   args <- getArgs
   mainWorker $ if "install" `elem` args || "configure" `elem` args
-                 then "--haste" : args
+                 then args ++ ["--haste"]
                  else args
 
 mainWorker :: [String] -> IO ()
